dbf543d9260692f26ce198768d45ffcc7392f5c7
rtaskayev <washingtonqwerty@gmail.com>
2022-10-26 23:05:29 +0200

    JavaCore-9. 2 - NASA API

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -8,13 +9,13 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import java.io.InputStream;
 
 public class Main {
 
-    public static final String REMOTE_SERVICE_URI = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+    public static final String REMOTE_SERVICE_URI = "https://api.nasa.gov/planetary/apod?api_key=aCmnh2Wsdfq52uj3oUDeFh8DAY80B3W6OnJlCkdg";
     public static final ObjectMapper mapper = new ObjectMapper();
 
     public static void main(String[] args) throws IOException {
@@ -31,13 +32,30 @@ public class Main {
         request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
 
         CloseableHttpResponse response = httpClient.execute(request);
-        Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
+        //Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
 
-        List<Post> posts = mapper.readValue(response.getEntity().getContent(), new TypeReference<>() {});
+        Post post = mapper.readValue(response.getEntity().getContent(), new TypeReference<>() {
+        });
+        //System.out.println(post);
+        System.out.println("URL - " + post.getUrl());
 
-        posts.stream()
-                .filter(value -> value.getUpvotes() > 0)
-                .forEach(System.out::println);
+        String remoteFileUri = post.getUrl();
+        String[] tokens = remoteFileUri.split("/");
+        String fileName = tokens[tokens.length - 1];
+        System.out.println("File Name - " + fileName);
+
+        HttpGet getImage = new HttpGet(remoteFileUri);
+        CloseableHttpResponse response2 = httpClient.execute(getImage);
+        HttpEntity entity = response2.getEntity();
+
+        InputStream is = entity.getContent();
+        FileOutputStream fos = new FileOutputStream(fileName);
+        int inByte;
+        while ((inByte = is.read()) != -1) {
+            fos.write(inByte);
+        }
+        is.close();
+        fos.close();
 
     }
 
--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -2,52 +2,49 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Post {
 
-    private final String id;
-    private final String text;
-    private final String type;
-    private final String user;
-    private final int upvotes;
+    private final String copyright;
+    private final String date;
+    private final String explanation;
+    private final String hdurl;
+    private final String media_type;
+    private final String service_version;
+    private final String title;
+    private final String url;
+
     public Post(
-            @JsonProperty("id") String id,
-            @JsonProperty("text") String text ,
-            @JsonProperty("type") String type,
-            @JsonProperty("user") String user,
-            @JsonProperty("upvotes") int upvotes
+            @JsonProperty("copyright") String copyright,
+            @JsonProperty("date") String date,
+            @JsonProperty("explanation") String explanation,
+            @JsonProperty("hdurl") String hdurl,
+            @JsonProperty("media_type") String media_type,
+            @JsonProperty("service_version") String service_version,
+            @JsonProperty("title") String title,
+            @JsonProperty("url") String url
     ) {
-        this.id = id;
-        this.text = text;
-        this.type = type;
-        this.user = user;
-        this.upvotes = upvotes;
+        this.copyright = copyright;
+        this.date = date;
+        this.explanation = explanation;
+        this.hdurl = hdurl;
+        this.media_type = media_type;
+        this.service_version = service_version;
+        this.title = title;
+        this.url = url;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public int getUpvotes() {
-        return upvotes;
+    public String getUrl() {
+        return url;
     }
 
     @Override
     public String toString() {
         return "Post" +
-                "\n id=" + id +
-                "\n text=" + text +
-                "\n type=" + type +
-                "\n user=" + user +
-                "\n upvotes=" + upvotes;
+                "\n copyright=" + copyright +
+                "\n date=" + date +
+                "\n explanation=" + explanation +
+                "\n hdurl=" + hdurl +
+                "\n media_type=" + media_type +
+                "\n service_version=" + service_version +
+                "\n title=" + title +
+                "\n url=" + url;
     }
 }
